caa2453c86787cee05a3309a7cfc944bc3ea8bf1
radwa <radwasaeed201@gmail.com>
2023-07-17 17:08:10 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newapp/screen/Home/Home_screen.dart';
 import 'package:newapp/shared/style/theme/MyThemeData.dart';
-
+import 'package:newapp/shared/style/component/News_Title.dart';
 void main() {
   runApp(MyApp());
 }
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: {
         Home_screen.routeName: (context) => Home_screen(),
+        News_Title.routeName:(_)=>News_Title(),
       },
       initialRoute: Home_screen.routeName,
       theme: MyThemeData.lightTheme,
